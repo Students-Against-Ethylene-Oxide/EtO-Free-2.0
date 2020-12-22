089026140d91903a64758c0dfcab4a935bf011e5
Elyssa <coding.guru16@gmail.com>
2020-12-22 00:23:10 -0600

    added 'new posts' section to home page

--- index.js
+++ index.js
@@ -1,205 +1,46 @@
-var filters = [];
+document.addEventListener("DOMContentLoaded", () => {
+	cardrow = document.getElementById("posts-cards");
+	let firebaseRef = firebase
+		.database()
+		.ref("1CWF0D-CWs-MV4Kr1qCJSyUk0EVzy7LeRsn9WMrIT7BA/Sheet1/");
 
-sortBy("data-name");
+	// Attach an asynchronous callback to read the data at our posts reference
+	firebaseRef.on(
+		"value",
+		function (snapshot) {
+			// Setting firebase data to a variable called "data"
+			data = snapshot.val();
+			dataArray = [];
 
-function sortBy(attr) {
-	cards = document.getElementsByClassName("review");
-	// magically coerce into an array first
-	cards = Array.prototype.slice.call(cards);
+			idArray = Object.keys(data);
+			dataArrayValues = Object.values(data);
 
-	cards.sort(function (a, b) {
-		return a.getAttribute(attr).localeCompare(b.getAttribute(attr));
-	});
-	for (var i = 0; i < cards.length; i++) {
-		// store the parent node so we can reattach the item
-		var parent = cards[i].parentNode;
-		// detach it from wherever it is in the DOM
-		var detatchedItem = parent.removeChild(cards[i]);
-		// reattach it.  This works because we are iterating
-		// over the items in the same order as they were re-
-		// turned from being sorted.
-		parent.appendChild(detatchedItem);
-	}
-}
-
-// // function change(selector) {
-//    element = document.querySelector(selector)
-//   console.log(element)
-//   if (element.classList) {
-//     element.classList.toggle("change")
-//   } else {
-//     // For IE9
-//     var classes = element.className.split(" ")
-//     var i = classes.indexOf("change")
-
-//     if (i >= 0)
-//       classes.splice(i, 1)
-//     else
-//       classes.push("change")
-//     element.className = classes.join(" ")
-//   }
-// }
-
-// function changeThis() {
-//   console.log(this)
-//   if (this.classList) {
-//     this.classList.toggle("change")
-//   } else {
-//     // For IE9
-//     var classes = this.className.split(" ")
-//     var i = classes.indexOf("change")
-
-//     if (i >= 0)
-//       classes.splice(i, 1)
-//     else
-//       classes.push("change")
-//     this.className = classes.join(" ")
-//   }
-// }
-
-function filterSearch() {
-	let input, cards, content;
-	input = document.getElementById("filter-input").value.trim();
-	cards = document.getElementsByClassName("review");
-    
-    for (i = 0; i < cards.length; i++) {
-        cards[i].classList.remove('d-flex');
-        cards[i].classList.add("d-none");
-    }
-
-	for (i = 0; i < cards.length; i++) {
-		content = cards[i].innerHTML.toLowerCase();
-        content = content.replace(/(<([^>]+)>)/gi, "");
-
-        if (content.includes(input)) {
-            cards[i].classList.remove("d-none");
-            cards[i].classList.add("d-flex");
-        } else {
-            cards[i].classList.remove("d-flex");
-			cards[i].classList.add("d-none");
-        }
-	}
-}
-
-function filter(attr, attrValue) {
-	// declare variables
-	cards = document.getElementsByClassName("review");
-	selector = `[${attr}=${attrValue}]`;
-    console.log(this.getAttribute("data-click") == "no");
-	// if the filter has not already been clicked, apply filter
-    if (this.getAttribute("data-click") == "no") {
-        // add the filter to an array of applied filters
-        // set the 'clicked' property to yes
-        filters.push(selector);
-        this.setAttribute("data-click", "yes");
-
-        // hide all the cards
-        for (i = 0; i < cards.length; i++) {
-            cards[i].classList.remove("d-flex");
-            cards[i].classList.add("d-none");
-        }
-
-        // for each filter, show any cards that apply
-        for (i = 0; i < filters.length; i++) {
-            selected = document.querySelectorAll(filters[i]);
-
-            for (a = 0; a < selected.length; a++) {
-                selected[a].classList.remove("d-none");
-                selected[a].classList.add("d-flex");
-            }
-        }
-    } else {
-        // get index of the filter and remove it from the array
-        // set the 'clicked' property to yes
-        index = filters.indexOf(selector);
-        filters.splice(index, 1);
-        this.setAttribute("data-click", "no");
-        console.log(index, filters);
-        
-        // if the array is not empty, repeat the above filtering process
-        if (filters.length != 0) {
-            // hide all the cards
-            for (i = 0; i < cards.length; i++) {
-                cards[i].classList.remove("d-flex");
-                cards[i].classList.add("d-none");
-            }
-
-            // for each filter, show any cards that apply
-            for (i = 0; i < filters.length; i++) {
-                // get a list of all the cards with that filter
-                selected = document.querySelectorAll(filters[i]);
-                console.log(selected);
-                for (a = 0; a < selected.length; a++) {
-                    selected[a].classList.toggle("d-none");
-                    selected[a].classList.toggle("d-flex");
-                }
-            }
-        // if the array is empty, show all the cards
-        } else {
-			// show all the cards
-			for (i = 0; i < cards.length; i++) {
-				cards[i].classList.remove("d-none");
-				cards[i].classList.add("d-flex");
+			// Tables are repeating user information when firebase information updates.
+			// cardrow.innerHTML = "";
+            for (let i = idArray.length - 1; i > idArray.length - 3; i--) {
+                date = new Date(dataArrayValues[i].date)
+                console.log(Date);
+				$("#posts-cards").append(
+					`<div class="col-md-6">
+                        <div class="card flex-md-row mb-4 box-shadow h-md-250">
+                            <div class="card-body d-flex flex-column align-items-start w-50">
+                                <h4 class="mb-0">
+                                    <a class="text-dark" href="reviews.html#${idArray[i]}">${dataArrayValues[i].name}</a>
+                                </h4>
+                                <div class="mb-1 text-muted">${date.toDateString()}</div>
+                                <a href="reviews.html#${idArray[i]}">Continue reading</a>
+                            </div>
+                            <div class="w-50">
+                                <img class="card-img-right img-fluid flex-auto d-none d-md-block" src="assets/images/${idArray[i]}.jpeg" onerror="this.onerror = null; this.src='assets/images/${idArray[i]}.jpg'"
+                                alt="Card image cap">
+                            </div>
+                        </div>
+                    </div>`
+				);
 			}
+		},
+		function (errorObject) {
+			console.log("The read failed: " + errorObject.code);
 		}
-    }
-}
-
-function clearFilters() {
-    // get all applicable elements
-    cards = document.getElementsByClassName("review");
-    tags = document.getElementsByClassName("tag");
-
-    // clear the search bar and filter array
-	filters = [];
-    document.getElementById("filter-input").value = "";
-    
-    // show all the cards
-	for (var i = 0; i < cards.length; i++) {
-        cards[i].classList.remove("d-none");
-        cards[i].classList.add("d-flex");
-    }
-    
-    // reset the filter buttons
-	for (var i = 0; i < tags.length; i++) {
-		tags[i].setAttribute("data-click", "no");
-    }
-}
-
-function showHint(str) {
-	if (str.length == 0) {
-		document.getElementById("txtHint").innerHTML = "";
-		return;
-	} else {
-		var xmlhttp = new XMLHttpRequest();
-		xmlhttp.onreadystatechange = function () {
-			if (this.readyState == 4 && this.status == 200) {
-				document.getElementById(
-					"txtHint"
-				).innerHTML = this.responseText;
-			}
-		};
-		xmlhttp.open("GET", "search.php?q=" + str, true);
-		xmlhttp.send();
-	}
-}
-
-function createModal() {
-    var title, content, text;
-
-    // get info from selected card
-    title = this.querySelector("h5").innerText;
-    content = this.querySelector('.content').innerHTML;
-
-    // create the modal and add all necessary attributes, classes, and info
-    modal = document.createElement("div");
-    modal.id = "modal";
-    modal.className = "modal fade";
-    modal.tabIndex = "-1";
-    modal.role = "dialog";
-    modal.innerHTML = `<div class="modal-dialog modal-lg modal-dialog-centered" role="document"><div class="modal-content"> <div class="modal-header"> <h5 class="modal-title" id="exampleModalLabel">${title}</h5> <button type="button" class="close" data-dismiss="modal" aria-label="Close"  onclick="modal.remove();"> <span aria-hidden="true">&times;</span> </button> </div> <div class="modal-body d-flex flex-column">${content}</div> <div class="modal-footer"> <button type="button" class="btn btn-secondary" data-dismiss="modal" onclick="modal.remove();">Close</button> </div> </div> </div>`;
-    document.body.appendChild(modal); 
-
-    // open the modal
-    $("#modal").modal();
-}
+	);
+});
